ae3f1e88d9fd3f0481b93b60bb14d51931b5826d
riordansantos <riordan3607@gmail.com>
2021-07-20 10:59:14 -0300

    feat(): select das perguntas lidas e não lidas

--- src/controllers/QuestionController.js
+++ src/controllers/QuestionController.js
@@ -1,5 +1,8 @@
+const Database = require("../db/config")
+
 module.exports ={
-    index(req,res){
+    
+    index(req, res){
         const roomId = req.params.room
         const questionId = req.params.question
         const action = req.params.action
@@ -7,5 +10,23 @@ module.exports ={
         
         console.log(`room = ${roomId}, questionId = ${questionId}, action = ${action}, password = ${password} `)
 
+    },
+
+    async create(req, res){
+        const db = await Database()
+        const question = req.body.question
+        const roomId = req.params.room
+
+        await db.run(`INSERT INTO questions(
+            title,
+            room,
+            read 
+        )VALUES (
+            "${question}",
+            ${roomId},
+            0
+        )`)
+
+        res.redirect(`/room/${roomId}`)
     }
 }
--- src/route.js
+++ src/route.js
@@ -7,9 +7,11 @@ const route = express.Router()
 route.get('/', (req, res) => res.render("index", {page: 'enter-room'}))
 route.get('/create-pass', (req, res) => res.render("index", {page: 'create-pass'}))
 
-route.get('/room/:room', (req, res)=> res.render("room"))
+route.post('/create-room', RoomController.create)
+route.get('/room/:room', RoomController.open)
 
 //formato que o formulario de dentro da modal passa as informacoes
+route.post('/question/create/:room', QuestionController.create)
 route.post('/question/:room/:question/:action', QuestionController.index)
-route.post('/create-room', RoomController.create)
+
 module.exports = route
