b8f2371b8d86312d56aa1adabaea28be675482c3
riordansantos <riordan3607@gmail.com>
2021-07-20 12:10:24 -0300

    feat(): marcar como lida e excluir pergunta

--- src/route.js
+++ src/route.js
@@ -9,9 +9,11 @@ route.get('/create-pass', (req, res) => res.render("index", {page: 'create-pass'
 
 route.post('/create-room', RoomController.create)
 route.get('/room/:room', RoomController.open)
+route.post('/enterroom', RoomController.enter)
 
 //formato que o formulario de dentro da modal passa as informacoes
 route.post('/question/create/:room', QuestionController.create)
 route.post('/question/:room/:question/:action', QuestionController.index)
+
 
 module.exports = route
