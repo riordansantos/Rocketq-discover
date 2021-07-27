8e5f190ee4c6bb4d8c12cc81090b017cad06574f
riordansantos <riordan3607@gmail.com>
2021-07-27 10:05:33 -0300

    tela de login

--- src/route.js
+++ src/route.js
@@ -6,6 +6,7 @@ const route = express.Router()
 
 route.get('/', (req, res) => res.render("index", {page: 'enter-room'}))
 route.get('/create-pass', (req, res) => res.render("index", {page: 'create-pass'}))
+route.get('/login', (req, res) => res.render('login'))
 
 route.post('/create-room', RoomController.create)
 route.get('/room/:room', RoomController.open)
