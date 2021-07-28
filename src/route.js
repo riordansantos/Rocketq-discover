6e39f8519b0cdc45743961c2003bfabc2492cad2
riordansantos <riordan3607@gmail.com>
2021-07-28 10:22:39 -0300

    novas telas e dependências

--- src/route.js
+++ src/route.js
@@ -7,6 +7,8 @@ const route = express.Router()
 route.get('/', (req, res) => res.render("index", {page: 'enter-room'}))
 route.get('/create-pass', (req, res) => res.render("index", {page: 'create-pass'}))
 route.get('/login', (req, res) => res.render('login'))
+route.get('/signup',(req, res) => res.render('signup'))
+route.get('/myrooms', (req, res) => res.render('myrooms'))
 
 route.post('/create-room', RoomController.create)
 route.get('/room/:room', RoomController.open)
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const route = require('./route')
 const path = require('path')
+const bodyParser = require('body-parser')
 
 const server = express()
 
@@ -13,5 +14,8 @@ server.set('views', path.join(__dirname, 'views'))
 server.use(express.urlencoded({extended:true}))
 
 server.use(route)
+
+server.use(bodyParser.json())
+server.use(bodyParser.urlencoded({ extended: false}))
 
 server.listen(3001, () => console.log("RODANDO"))
